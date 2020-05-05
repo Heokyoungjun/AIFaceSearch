f6c907e6ff92077c0ffdcb2129f5beb46f2e568b
Kyoungjun Heo <ggaia.heo@gmail.com>
2020-05-05 21:25:38 +0000

    내용 수정 1.0.34

--- js/topmenubar.js
+++ js/topmenubar.js
@@ -2,12 +2,12 @@ $(document).ready(function(){
 	$('.sub-menu').hide();
 	
 	$('#principale li').mouseover(function(){
-		$('.sub-menu', this).stop().slideDown(1000, 'easeOutElastic');
+		$('.sub-menu', this).stop().slideDown(0, 'easeOutElastic');
 		$(this).addClass('activate');
 	});
 	
 	$('#principale li').mouseleave(function(){
-		$('.sub-menu', this).slideUp(1000, 'easeInElastic');
+		$('.sub-menu', this).slideUp(0, 'easeInElastic');
 		$(this).removeClass('activate');
 	});
 	
